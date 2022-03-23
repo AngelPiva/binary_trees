255e79791ebfebbda61ef09626d3734dca5ce1f9
Marcosozhol <3956@holbertonschool.com>
2022-03-23 07:55:49 -0700

    Correction coments in code

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -31,6 +31,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	return (bin_height(tree) - 1);
 }
 
+/**
+ *binary_tree_balance - measures the balance factor of a binary tree
+ *@tree: pointer to the root node of the tree to measure the balance factor
+ *Return: int
+ */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int aux1, aux2;
